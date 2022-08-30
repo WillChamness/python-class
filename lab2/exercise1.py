a5d0afd11a6ee425f8e06407ade32d157770d081
WillChamness <wzchamness@gmail.com>
2022-08-30 17:09:37 -0400

    add exercise

---
+++ lab2/exercise1.py
@@ -0,0 +1,10 @@
+name = input("enter your name: ")
+street_addr = input("enter your street address: ")
+city = input("enter the city: ")
+state = input("enter the state: ")
+zip = input("enter the ZIP code: ")
+phone = input("enter your telephone number: ")
+major = input("enter your college major: ")
+
+print(name, street_addr + ",", city + ",", state, zip, phone, major)
+
